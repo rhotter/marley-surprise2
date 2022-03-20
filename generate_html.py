157a20da8fc05cc4e9ed3ba336aa25231a9b7dc2
Raffi Hotter <rhotter@users.noreply.github.com>
2022-03-20 01:44:49 -0400

    images generated

--- generate_html.py
+++ generate_html.py
@@ -1,15 +1,25 @@
 import json
+from os import listdir
+from os.path import isfile, join
+import random
 
-f = open("messages.json","r")
+f = open("messages.json", "r")
 messages = json.load(f)["messages"]
 f.close()
 
-for i in messages:
-    name, message = i["name"],i["message"]
+images_path = "images"
+images = [
+    f
+    for f in listdir(images_path)
+    if isfile(join(images_path, f)) and f.endswith(".png")
+]
+random.shuffle(images)
+
+for i, msg in enumerate(messages):
+    name, message = msg["name"], msg["message"]
     f = open(name + ".html", "w")
 
-
-    contents = """<!DOCTYPE html>
+    contents = f"""<!DOCTYPE html>
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -20,27 +30,25 @@ for i in messages:
     </head>
     <body>
         <div class="container-sm">
-            
+
             <h1 class="name">
                 <span class="highlight">
-                {nm}
+                {name}
             </span>
             </h1>
             <div class="textBody">
-                {msg}
+                {message}
             </div>
             <div class="back">
                 <a class="backA" href="./index.html">Back</a>
             </div>
+            <div style="padding-top: 30px;">
+            <img src="images/{images[i]}" width="100%">
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
-</html>
-""".format(nm=name,msg=message)
-
+</html>"""
 
     f.write(contents)
     f.close()
     print("done writing {}.html".format(name))
-
-
