7eb02110e23cc3b4432e3d795716889d8add8e96
Stephen Fay <dcxstephen@live.co.uk>
2022-03-20 01:04:50 -0400

    added html template to gen pages script

--- generate_html.py
+++ generate_html.py
@@ -8,18 +8,35 @@ for i in messages:
     name, message = i["name"],i["message"]
     f = open(name + ".html", "w")
 
-    contents = """<DOCTYPE html>
+
+    contents = """<!DOCTYPE html>
 <html>
-<head>
-
-</head>
-<body>
-    <h1>{nm}</h1>
-    <p>{msg}</p>
-</body>
-
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Definitely Curius.app</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="page.css">
+    </head>
+    <body>
+        <div class="container-sm">
+            
+            <h1 class="name">
+                <span class="highlight">
+                {nm}
+            </span>
+            </h1>
+            <div class="textBody">
+                {msg}
+            </div>
+            <div class="back">
+                <a class="backA" href="./index.html">Back</a>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 </html>
-    """.format(nm=name,msg=message)
+""".format(nm=name,msg=message)
 
 
     f.write(contents)
