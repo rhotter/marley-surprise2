83ec9cf714b6e94ad72bb28d10f277ccd0773ee5
Stephen Fay <dcxstephen@live.co.uk>
2022-03-20 00:36:54 -0400

    generate html added

---
+++ generate_html.py
@@ -0,0 +1,30 @@
+import json
+
+f = open("messages.json","r")
+messages = json.load(f)["messages"]
+f.close()
+
+for i in messages:
+    name, message = i["name"],i["message"]
+    f = open(name + ".html", "w")
+
+    contents = """<DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <h1>{nm}</h1>
+    <p>{msg}</p>
+</body>
+
+</html>
+    """.format(nm=name,msg=message)
+
+
+    f.write(contents)
+    f.close()
+    # print("---")
+    # print(i["name"])
+    # print(i["message"])
+    print("done")
