8d1bd011b955d959704ddffd383249a599fe438c
Stephen Fay <dcxstephen@live.co.uk>
2022-03-20 02:11:39 -0400

    updated title

--- generate_html.py
+++ generate_html.py
@@ -25,7 +25,7 @@ for i, msg in enumerate(messages):
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <title>Definitely Curius.app</title>
+        <title>{name}</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="page.css">
